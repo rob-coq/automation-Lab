65edd4371d550d0a7abd397f39a713ffe9310274
rob-coq <44269196+rob-coq@users.noreply.github.com>
2021-08-19 15:10:19 +0200

    Update variables.tf

--- terraform-CD/variables.tf
+++ terraform-CD/variables.tf
@@ -4,6 +4,13 @@ variable region {
     default = "us-east-1"
 }
 
+variable access_key {
+    description = "AWS access key"
+}
+variable secret_key {
+    description = "AWS secret key"
+}
+
 variable "owner" {
     description = "Owner of the AMI"
     default = "self"
